b6317d3ce81e0b4ca0600482a9e9199fbd18a9e8
홍승택 <tmdxor66@naver.com>
2021-03-18 00:04:22 +0900

    Axios 사용하여 서버에서 데이터 받아 화면에 출력 하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import axios from 'axios';
+
+
 
 function App() {
+  axios.defaults.baseURL = "http://localhost:8080";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Content></Content>
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="title">타아이틀</div>
+    </div>
+  );
+}
+ 
+function Content() {
+  const [list, setList] = useState([
+    {
+      boardId: 11,
+      content: "-",
+      insertDate: "-",
+      title: "-"
+    },
+    {
+      boardId: 22,
+      content: "-",
+      insertDate: "-",
+      title: "-"
+    }
+  ]);
+
+  function fnSearch() {
+    axios.post('board/list',{}).then(function(data) {
+      setList(data.data);
+    });
+  }
+
+  return (
+    <div className="content">
+      <div className="list">
+        <button className="btn" onClick={fnSearch}>조회</button>
+        <BoardList list={list}/>
+      </div>
+    </div>
+  );
+}
+
+function BoardList({list}) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>제목</th>
+          <th>내용</th>
+          <th>만든날짜</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map(item => (
+          <BoardItem data={item} key={item.boardId}/>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function BoardItem({data}) {
+  return (
+    <tr>
+      <td>{data.boardId}</td>
+      <td>{data.title}</td>
+      <td>{data.content}</td>
+      <td>{data.insertDate}</td>
+    </tr>
   );
 }
 
