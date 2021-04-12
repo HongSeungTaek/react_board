8c128bf3820cec6bf26fa44c11cf03e533c8dfda
홍승택 <tmdxor66@naver.com>
2021-04-13 00:42:42 +0900

    수정 / 삭제 기능 구현

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import Const from '../Const';
 import CommonUtil from '../CommonUtil';
 
-function Detail() {
+function Detail({ location, history }) {
   let { boardId } = useParams();
   const [pageType, setPageType] = useState('D');
   const [title, setTitle] = useState('');
@@ -23,18 +23,27 @@ function Detail() {
     });
   }
 
-  getOne();
-  useEffect(() => {
-    console.log(11);
-  });
-
-  const fnMod = () => {
-    setPageType('M');
+  if(pageType == 'D') {
+    getOne(); 
   }
 
-  const fnCancel = () => {
-    setPageType('D');
-    getOne();
+  const fnSave = () => {
+    axios.post('board/mod', {
+      'boardId': boardId,
+      'title': title,
+      'content': content
+    }).then(function(res) {
+      setPageType('D');
+    });
+  }
+  const fnDelete = () => {
+    if(window.confirm('삭제하시겠습니까?')) {
+      axios.post('board/del', {
+        'boardId': boardId
+      }).then(function(res) {
+        history.push('/');
+      });
+    }
   }
 
   return(
@@ -54,10 +63,10 @@ function Detail() {
           ?content
           :<textarea value={content} onChange={({ target: { value }}) => setContent(value) }/>}
         </div>
-        {pageType == 'D'
-        ?<button onClick={fnMod}>수정</button>
-        :<button onClick={fnCancel}>취소</button>
-        }
+        {pageType == 'D' && <button onClick={() => { setPageType('M'); }}>수정</button>}
+        {pageType == 'D' && <button onClick={fnDelete}>삭제</button>}
+        {pageType == 'M' && <button onClick={() => { setPageType('D'); }}>취소</button>}
+        {pageType == 'M' && <button onClick={fnSave}>저장</button>}
       </div>
     </div>
   );
